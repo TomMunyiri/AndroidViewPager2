3c8096e72ee064cc25ac4f5c4b0e44b36b0200a3
tom <tmmunyiri@gmail.com>
2021-01-25 21:08:39 +0300

    changes

--- app/src/main/java/com/tommunyiri/androidviewpager2/MainActivity.kt
+++ app/src/main/java/com/tommunyiri/androidviewpager2/MainActivity.kt
@@ -2,10 +2,37 @@ package com.tommunyiri.androidviewpager2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var onboardingItemsAdapter: OnboardingItemsAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setOnboardingItems()
+    }
+
+    private fun setOnboardingItems(){
+        onboardingItemsAdapter= OnboardingItemsAdapter(
+            listOf(
+                OnboardingItem(
+                    onboardingImage = R.drawable.ic_task,
+                    title = "Manage your task",
+                    description = "Organize all your TODOs and projects. Color tag them to set priorities and categories."
+                ),
+                OnboardingItem(
+                    onboardingImage = R.drawable.ic_time,
+                    title = "Work on time",
+                    description = "When you're overwhelmed by amount of work you have on your plate, stop and rethink."
+                ),
+                OnboardingItem(
+                    onboardingImage = R.drawable.ic_reminder,
+                    title = "Get reminder on time",
+                    description = "When you encounter a small task that takes less than 5 minutes to complete."
+                )
+            )
+        )
+        val onboardingViewPager=findViewById<ViewPager2>(R.id.onboardingViewPager)
+        onboardingViewPager.adapter=onboardingItemsAdapter
     }
 }
---
+++ app/src/main/java/com/tommunyiri/androidviewpager2/OnboardingItem.kt
@@ -0,0 +1,7 @@
+package com.tommunyiri.androidviewpager2
+
+data class OnboardingItem(
+    val onboardingImage: Int,
+    val title: String,
+    val description: String
+)
